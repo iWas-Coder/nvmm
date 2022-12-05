407ff32f22541e5d6240d9765bd9027001997f1b
iWas-Coder <wasymatieh01@gmail.com>
2022-12-05 20:04:35 +0100

    Fixed INSTALL; Prepared src dir and include dir

---
+++ src/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+
+void hello_world(char* str) {
+    printf("Hello, %s\n", str);
+}
---
+++ src/include/hello.h
@@ -0,0 +1,8 @@
+#ifndef HELLO_H_
+#define HELLO_H_
+
+
+void hello_world(char* str);
+
+
+#endif
